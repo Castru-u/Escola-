df2d788f5e57ca1c356ba0e0235f017f27f9370c
Castru-u <joaopedrocs1010@gmail.com>
2023-05-29 20:00:17 -0300

    funcoes

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,26 @@
-def cadastro(parametro=""):
-    if parametro == "":
-        print("Parametro não informado (ALUNO/TURMA/BOLETIM)")
+def cadastro(parametro):
+
+    #Abre os arquivos a serem usados
+    with open("alunos.txt", "a", encoding="utf-8") as alunos, open("turmas.txt", "a", encoding="utf-8") as turmas, open("boletins.txt", "a", encoding="utf-8") as boletins:
+
+        #Verificam o parametro inserido:
+
+        if parametro.upper() not in ["ALUNO","TURMA","BOLETIM"]:
+            #Caso não seja informado um dos parametros acima
+            print("Parametro não informado (ALUNO/TURMA/BOLETIM)")
+
+        elif parametro.upper() == "ALUNO":
+            #Pede as informações do aluno a ser inserido
+            aluno=input("Insira o nome do(a) aluno(a): ")
+            email=input("Insira o email do(a) aluno(a): ")
+            telefone=int(input("Insira o telefone do responsável do(a) aluno(a): "))
+            turma=int(input("Insira o código de turma: "))
+
+            matricula=1+alunos.readlines()
+
+                arquivo.write(f"{matricula} | {aluno} | {email} | {telefone}")
+
+
+    
+
+
