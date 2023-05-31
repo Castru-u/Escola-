ecb3468c274bb09daa375c79264c89b2d34da95a
Castru <131192586+Castru-u@users.noreply.github.com>
2023-05-31 15:02:32 -0300

    funcao cadastro completa.py

--- funcoes.py
+++ funcoes.py
@@ -30,7 +30,41 @@ def cadastro(parametro):
 
                 alunos.write(f"{matricula} | {aluno} | {email} | {telefone} | {turma}\n")   #Adiciona o aluno
 
+        elif parametro.upper() == "TURMA":
+            #pede as informações da turma a ser cadastrada
+            nome=input("Insira o nome da turma: ")
+            turno=(input("Insira o turno da turma(M/T/N/I): ")).upper()
+            if turno not in ["M","T","N","I"]:
+                print("Turno inserido não existe")
+            else:
+                ano=input("Insira o ano da turma(1-9): ")
+                if int(ano) not in range(1,10):
+                    print("Ano inserido não existe")
+                else:
+                    turmas.seek(0,0)
+                    linhas=turmas.readlines()
+                    idturma=1+int(linhas[-1].split(" | ")[0]) if linhas!=[] else 1   #Cria o ID para a turma
 
-    
+                    turmas.write(f"{idturma} | {nome} | {turno} | {ano}°\n")   #Adiociona a turma
 
+        elif parametro.upper() == "BOLETIM":
+            #pede as informações do boletim a ser inserido
+            idaluno=input("Insira a matrícula do aluno a ser inserido o boletim")
+            alunos.seek(0,0)
+            if idaluno not in [i.split(" | ")[0] for i in alunos.readlines()]:
+                print("Matrícula inserida não existe entre os alunos no sistema")
+            else:
+                nota1=float(input("Insira a 1° nota: "))
+                nota2=float(input("Insira a 2° nota: "))
+                nota3=float(input("Insira a 3° nota: "))
+                nota4=float(input("Insira a 4° nota: "))
+                faltas=input("Insira o n° de faltas: ")
+                situ=""
+                if mean([nota1,nota2,nota3,nota4])>=7 and int(faltas)<15:   #Vai atribuir a situação do aluno dependendo de suas notas e faltas
+                    situ="Aprovado"
+                else:
+                    situ="Reprovado"
 
+                idboletim=1+int(linhas[-1].split(" | ")[0]) if linhas!=[] else 1   #Cria o ID para o boletim
+
+                turmas.write(f"{idboletim} | {idaluno} | {[nota1,nota2,nota3,nota4]} | {faltas} | {situ}\n")   #Adiociona o boletim
